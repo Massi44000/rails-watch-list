bfffe26ed04fa29c4560394ee13b71148630c5dc
Macenissa Mondeguer <mmondeguer@gmail.com>
2024-08-09 15:47:23 +0200

    watch-list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,8 @@
 class List < ApplicationRecord
-  has_one_attached :photo
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
+  has_one_attached :photo
   validates :name, presence: true
   validates :name, uniqueness: true
+  validates :photo, presence: true
 end
