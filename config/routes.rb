b719021fbf96a77eaa5226bc8f0b1280583b7926
Macenissa Mondeguer <mmondeguer@gmail.com>
2024-08-09 11:23:44 +0200

    bbh

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  resources :lists, only: %i[index new create show] do
-    resources :bookmarks, only: %i[index new create destroy]
-  end
+  resources :lists, only: %i[index new create show] 
 end
